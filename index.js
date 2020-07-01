0a5d595ae093bc134a1280ac47ef30df6ddeb12b
nhatpham <nhatphamkio@gmail.com>
2020-07-01 23:35:17 +0900

    change color socket.io

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ app.set("view engine","ejs");
 app.set("views","./views");
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-server.listen(4000);
+server.listen(8001);
 //on listen
 io.on("connection",function(socket){
     console.log("co nguoi ket noi" + socket.id);
-    
+    socket.on("Client-send-color",function(data){
+        console.log(data);
+        io.sockets.emit("Server-send-color",data);
+    });    
 });
 app.get("/",function(req,res){
     res.render("trangchu");
