98615280a0fee4dbaa7015e397c00e830f77d577
nhatpham <nhatphamkio@gmail.com>
2020-07-01 23:13:13 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express")
+var app = express();
+app.use(express.static("./public"));
+app.set("view engine","ejs");
+app.set("views","./views");
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(4000);
+//on listen
+io.on("connection",function(socket){
+    console.log("co nguoi ket noi" + socket.id);
+    
+});
+app.get("/",function(req,res){
+    res.render("trangchu");
+})
